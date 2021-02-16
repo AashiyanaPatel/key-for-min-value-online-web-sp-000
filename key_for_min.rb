e092a9e5dae72c1affc1d726cbabaeb5ccf1e43c
Aashiyana Patel <aashiyanapatel@rocketmail.com>
2021-02-16 02:38:52 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -28,11 +28,10 @@ end 	enddef key_for_min_value(name_hash)
       current_minimum_val = value 
       current_key_assoc_w_min_val = key 
       
-     
     elsif current_minimum_val > value 
         current_minimum_val = value
         current_key_assoc_w_min_val = key 
-      end
-  current_key_assoc_w_min_val 
-end 	
+    end
+    current_key_assoc_w_min_val 
+  end 	
 end
