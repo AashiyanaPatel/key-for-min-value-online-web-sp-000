3139dadf5fb23af1df7212508325d2fc0adeb8e7
Aashiyana Patel <aashiyanapatel@rocketmail.com>
2021-02-16 02:46:31 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -20,20 +20,3 @@ def key_for_min_value(name_hash)
   current_key_assoc_w_min_val 
 end 	
 
-def key_for_min_value(name_hash)
-
-  current_minimum_val = nil 
-  current_key_assoc_w_min_val = nil 
-  name_hash.each do |key, value|
-
-    if current_minimum_val == nil 
-      current_minimum_val = value 
-      current_key_assoc_w_min_val = key 
-      
-    elsif current_minimum_val > value 
-        current_minimum_val = value
-        current_key_assoc_w_min_val = key 
-    end
-    current_key_assoc_w_min_val 
-  end 	
-end
